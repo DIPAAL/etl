0b877faada6bbb57f23a45dc4c8da37275f3bf09
Lau Ernebjerg Josefsen <lejo@lejo-xps>
2022-10-07 12:40:49 +0200

    Setup core

--- main.py
+++ main.py
@@ -36,12 +36,17 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--init", help="Initialize database", action="store_true")
     parser.add_argument("--load", help="Perform loading of the dates", action="store_true")
+    parser.add_argument("--file", help="File to load")
     args = parser.parse_args()
 
     if args.init:
         wrap_in_timings("Database init", lambda: init_database(config))
 
     if args.load:
+        if args.file is None or not os.path.isfile(os.path.join(config['DataSource']['ais_path'], args.file)):
+            print("Please specify a valid file to load")
+            exit(2)
+
         wrap_in_timings("Loading", lambda: load_data(config))
 
 if __name__ == '__main__':
