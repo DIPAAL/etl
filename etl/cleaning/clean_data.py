b3d1106ec99270301902cc88a3ad862a23698649
Alex <alex.klitgaard@gmail.com>
2022-10-13 12:13:43 +0200

    Working on dask to make it very fast cleaning

--- etl/cleaning/clean_data.py
+++ etl/cleaning/clean_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import geopandas as gpd
-from etl.helper_functions import get_queries_from_sql_file
+import dask.dataframe as dd
+import dask_geopandas as d_gpd
+from etl.helper_functions import wrap_with_timings, get_first_query_in_file
 from sqlalchemy import create_engine
 
 CSV_EXTENSION = '.csv'
@@ -25,36 +27,39 @@ def _clean_csv_data(config, ais_file_path_csv: str) -> gpd.GeoDataFrame:
     
     # Use Geopandas and psycopg2 to get the Danish Waters geometry from the DB.
     # Then uses that to filter on latitude and longitude
-    danish_waters_gdf = _get_danish_waters_boundary(config)
+    danish_waters_gdf = wrap_with_timings('Fetch Danish Waters', lambda: _get_danish_waters_boundary(config))
 
     # Read from georeferenced AIS dataframe from csv file
-    dirty_geo_dataframe = _create_dirty_df_from_ais_cvs(csv_path=ais_file_path_csv, crs=COORDINATE_REFERENCE_SYSTEM)
+    dirty_geo_dataframe = wrap_with_timings('Create Geodataframe from CSV', lambda: _create_dirty_df_from_ais_cvs(csv_path=ais_file_path_csv, crs=COORDINATE_REFERENCE_SYSTEM))
 
     # Initial cleaning of AIS dataframe
-    initial_cleaned_dataframe = _ais_df_initial_cleaning(dirty_dataframe=dirty_geo_dataframe)
+    initial_cleaned_dataframe = wrap_with_timings('Initial clean', lambda: _ais_df_initial_cleaning(dirty_dataframe=dirty_geo_dataframe))
 
     # Do a spatial join (inner join) to find all the ships that is within the boundary of danish_waters 
-    clean_gdf = gpd.sjoin(initial_cleaned_dataframe, danish_waters_gdf)
-    print('Number of rows in boundary cleaned dataframe: ' + str(len(clean_gdf)))
+    initial_cleaned_dataframe = initial_cleaned_dataframe.set_crs(COORDINATE_REFERENCE_SYSTEM)
+    clean_gdf = wrap_with_timings('Spatial cleaning', lambda: d_gpd.sjoin(initial_cleaned_dataframe, danish_waters_gdf, predicate='within'))
+    print('Number of rows in boundary cleaned dataframe: ' + str(clean_gdf.size.compute()))
 
     return clean_gdf
 
 
 
-def _get_danish_waters_boundary(config) -> gpd.GeoDataFrame:
+def _get_danish_waters_boundary(config) -> d_gpd.GeoDataFrame:
     conn = _create_pandas_postgresql_connection(config)
 
-    query_gen = get_queries_from_sql_file(GEOMETRY_BOUNDS_QUERY)
-    query = query_gen.__next__()
-    return gpd.read_postgis(sql=query, con=conn)
+    query = get_first_query_in_file(GEOMETRY_BOUNDS_QUERY)
+    temp_waters = gpd.read_postgis(sql=query, con=conn)
+    return d_gpd.from_geopandas(data=temp_waters, npartitions=1)
 
-def _create_dirty_df_from_ais_cvs(csv_path: str, crs: str) -> gpd.GeoDataFrame:
-    dirty_frame = pd.read_csv(csv_path)
-    return gpd.GeoDataFrame(data=dirty_frame, geometry=gpd.points_from_xy(dirty_frame.Longitude, dirty_frame.Latitude), crs=crs)
+def _create_dirty_df_from_ais_cvs(csv_path: str, crs: str) -> d_gpd.GeoDataFrame:
+    dirty_frame = dd.read_csv(csv_path, dtype={'Callsign': 'object', 'Cargo type': 'object', 'Destination': 'object', 'ETA': 'object', 'Name': 'object'})
+    #dirty_frame = pd.read_csv(csv_path)
 
-def _ais_df_initial_cleaning(dirty_dataframe: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    print('Number of rows in dataframe before initial clean: ' + str(len(dirty_dataframe)))
-    dirty_dataframe.drop_duplicates()
+    # dirty_geopandas = gpd.GeoDataFrame(data=dirty_frame, geometry=gpd.points_from_xy(dirty_frame.Longitude, dirty_frame.Latitude), crs=crs)
+    return d_gpd.from_dask_dataframe(df=dirty_frame, geometry=d_gpd.points_from_xy(df=dirty_frame, x='Longitude', y='Latitude', crs=crs))
+
+def _ais_df_initial_cleaning(dirty_dataframe: d_gpd.GeoDataFrame) -> d_gpd.GeoDataFrame:
+    print('Number of rows in dataframe before initial clean: ' + str(dirty_dataframe.size.compute()))
     dirty_dataframe.query(expr=(
                                 '(Draught < 28.5 | Draught.isna()) & '
                                 '(Width < 75) & '
@@ -63,7 +68,7 @@ def _ais_df_initial_cleaning(dirty_dataframe: gpd.GeoDataFrame) -> gpd.GeoDataFr
                                 '(MMSI > 99999999) & '
                                 '(MMSI <= 111000000 | MMSI >= 112000000)'
                                 ), inplace=True)
-    print('Number of rows in dataframe after initial clean: ' + str(len(dirty_dataframe)))
+    print('Number of rows in dataframe after initial clean: ' + str(dirty_dataframe.size.compute()))
     return dirty_dataframe
 
 def _create_pandas_postgresql_connection(config):
