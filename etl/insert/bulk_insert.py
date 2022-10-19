0c1414094c904c2902aeb0290c9c01eebe02a216
Mikael Vind Mikkelsen <mimikk21@student.aau.dk>
2022-10-19 08:31:38 +0200

    Updated the function and cleanup.
    Moving on to create testing

--- etl/insert/bulk_insert.py
+++ etl/insert/bulk_insert.py
@@ -5,18 +5,15 @@ __author__ = "Mikael Vind Mikkelsen"
 __maintainer__ = ""
 
 import psycopg2
-import os
-import pandas as pd
 from io import StringIO
 from etl.helper_functions import get_connection
 
 
-def insert_trajectories(data_frame, target_table, config):
+def insert_bulk(data_frame, target_table, config):
     conn = get_connection(config)
     cursor = conn.cursor()
 
-    """Buffering the data frame as a CSV file
-    NOTE - PERFORMANCE: Very memory intensive, could cause issues"""
+    """Creates a buffer, converts the data frame into a CSV file and store it in the buffer"""
     buffer = StringIO()
     data_frame.to_csv(buffer, index_label='id', header=False)
     buffer.seek(0)
@@ -29,24 +26,6 @@ def insert_trajectories(data_frame, target_table, config):
         conn.rollback()
         cursor.close()
         return 1
+
     print("Insertion done")
     cursor.close()
-
-
-def insert_iterator_trajectories(data_frame, table, config):
-    conn = get_connection(config)
-    cursor = conn.cursor()
-
-
-def insert_cells(config):
-    """TODO: Created function, once work on trajectory rollup module commences"""
-    conn = get_connection(config)
-    return 0
-
-
-def _dataframe_buffer(data_frame):
-    """Saves dataframe to an in memory buffer"""
-
-    buffer = StringIO()
-    data_frame.to_csv(buffer, index_label='id', header=False)
-    return buffer.seek(0)
