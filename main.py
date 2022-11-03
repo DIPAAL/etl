827b6bedfb5145bb501deb0f6aea3283e6d15abc
Lau Josefsen <ljosef18@student.aau.dk>
2022-11-03 10:06:36 +0100

    Try import

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import argparse
 import configparser
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pandas as pd
 
@@ -33,32 +33,50 @@ def main(argv):
     parser.add_argument("--init", help="Initialize database", action="store_true")
     parser.add_argument("--load", help="Perform loading of the dates", action="store_true")
     parser.add_argument("--clean", help="Clean the given AIS data file", action="store_true")
-    parser.add_argument("--date", help="The date to load, in the format YYYY-MM-DD, for example 2022-12-31", type=str)
+    parser.add_argument("--from_date",
+                        help="The date to load from, in the format YYYY-MM-DD, for example 2022-12-31", type=str)
+    parser.add_argument("--to_date",
+                        help="The date to load to, in the format YYYY-MM-DD, for example 2022-12-31", type=str)
 
     args = parser.parse_args()
 
-    date = datetime.strptime(args.date, '%Y-%m-%d') if args.date else None
+    date_from = datetime.strptime(args.date, '%Y-%m-%d') if args.from_date else None
+    date_to = datetime.strptime(args.date, '%Y-%m-%d') if args.to_date else None
 
     if args.init:
         wrap_with_timings("Database init", lambda: init_database(config))
 
     if args.clean:
+        # ensure date_from and date_to is set
+        if not date_from or not date_to:
+            raise ValueError("Please provide both from_date and to_date when cleaning data")
 
-        file_path = wrap_with_timings(
-            "Ensuring file for current date exists",
-            lambda: ensure_file_for_date(date, config)
-        )
-        pickle_path = file_path.replace('.csv', '.pkl')
+        # ensure date_from is before date_to
+        if date_from > date_to:
+            raise ValueError("from_date must be before to_date")
 
-        if os.path.isfile(pickle_path):
-            print("Cached pickled data found..")
-            trajectories = pd.read_pickle(pickle_path)
-        else:
-            clean_sorted_ais = wrap_with_timings("Data Cleaning", lambda: clean_data(config, file_path))
-            trajectories = wrap_with_timings("Trajectory construction", lambda: build_from_geopandas(clean_sorted_ais))
-            trajectories.to_pickle(pickle_path)
+        # loop through all dates and clean them
+        while date_from <= date_to:
+            wrap_with_timings(f"Cleaning data for {date_from}", lambda: clean_date(date_from, config))
+            date_from += timedelta(days=1)
 
-        wrap_with_timings("Inserting trajectories", lambda: TrajectoryInserter().persist(trajectories, config))
+
+def clean_date(date: datetime, config):
+    file_path = wrap_with_timings(
+        "Ensuring file for current date exists",
+        lambda: ensure_file_for_date(date, config)
+    )
+    pickle_path = file_path.replace('.csv', '.pkl')
+
+    if os.path.isfile(pickle_path):
+        print("Cached pickled data found..")
+        trajectories = pd.read_pickle(pickle_path)
+    else:
+        clean_sorted_ais = wrap_with_timings("Data Cleaning", lambda: clean_data(config, file_path))
+        trajectories = wrap_with_timings("Trajectory construction", lambda: build_from_geopandas(clean_sorted_ais))
+        trajectories.to_pickle(pickle_path)
+
+    wrap_with_timings("Inserting trajectories", lambda: TrajectoryInserter().persist(trajectories, config))
 
 
 if __name__ == '__main__':
