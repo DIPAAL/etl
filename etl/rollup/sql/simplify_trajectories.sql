9c9d86cb25adac59d9d456ba83282657b14b8a51
Lau Josefsen <ljosef18@student.aau.dk>
2022-11-21 10:20:21 +0100

    Added simplify query

--- etl/rollup/sql/simplify_trajectories.sql
+++ etl/rollup/sql/simplify_trajectories.sql
@@ -1,1 +1,5 @@
-UPDATE dim_trajectory SET trajectory = transform(simplify(transform(setSRID(trajectory, 4326), 3034), 10, true), 4326);
+UPDATE dim_trajectory dt
+SET trajectory = transform(simplify(transform(setSRID(trajectory, 4326), 3034), 10, true), 4326)
+FROM fact_trajectory ft
+WHERE ft.trajectory_id = dt.trajectory_id AND
+    ft.start_date_id = %s;
