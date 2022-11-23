6541e7b92a82975ab596785667247d8230b23572
Mikael <mimikk21@student.aau.dk>
2022-11-23 13:25:54 +0100

    log_etl_stage_time method simplified

--- tests/audit_logger_test.py
+++ tests/audit_logger_test.py
@@ -5,6 +5,7 @@ import pytest
 import pandas as pd
 import geopandas as gpd
 import numpy as np
+from etl.constants import ETL_STAGE_CLEAN, ETL_STAGE_SPATIAL, ETL_STAGE_TRAJECTORY, ETL_STAGE_CELL, ETL_STAGE_BULK
 
 
 VERSION_NUMBERS_LIST = ['v1.0.2', 'version 2', 'final final', '2nd prototype', None, 'v1.22.12']
@@ -22,37 +23,26 @@ def test_audit_log_version_number(version_number):
 def test_audit_log_etl_stage():
     al = AuditLogger()
     start_time = 0
-    end_time = 42
+    end_time = 42.320393
     expected_time = end_time - start_time
     expected_rows = 100
-    start_datetime = datetime(2022, 11, 15, 11, 25, 0)
-    end_datetime = datetime(2022, 11, 15, 11, 25, 22)
-    expected_datetime = timedelta.total_seconds(end_datetime - start_datetime)
     dataframe_pandas = pd.DataFrame(index=np.arange(0, expected_rows), columns=['col1', 'col2', 'col3'])
     dataframe_geopandas = gpd.GeoDataFrame(index=np.arange(0, expected_rows), columns=['col1', 'col2', 'col3'])
 
-    al.log_etl_stage_time('cleaning', start_time, end_time)
-    al.log_etl_stage_rows_df('cleaning', dataframe_pandas)
-    al.log_etl_stage_time('spatial_join', start_datetime, end_datetime)
-    al.log_etl_stage_rows_df('spatial_join', dataframe_geopandas)
+    al.log_etl_stage_time(ETL_STAGE_CLEAN, start_time, end_time)
+    al.log_etl_stage_rows_df(ETL_STAGE_CLEAN, dataframe_pandas)
+    al.log_etl_stage_time(ETL_STAGE_SPATIAL, start_time, end_time)
 
-    assert al.log_dict['cleaning_delta_time'] == expected_time
-    assert al.log_dict['cleaning_rows'] == expected_rows
-    assert al.log_dict['spatial_join_delta_time'] == expected_datetime
-    assert al.log_dict['spatial_join_rows'] == expected_rows
-    assert al.log_dict['total_delta_time'] == expected_time + expected_datetime
+    assert al.log_dict[f'{ETL_STAGE_CLEAN}_delta_time'] == expected_time
+    assert al.log_dict[f'{ETL_STAGE_CLEAN}_rows'] == expected_rows
+    assert al.log_dict[f'{ETL_STAGE_SPATIAL}_delta_time'] == expected_time
+    assert al.log_dict['total_delta_time'] == expected_time * 2
 
     al.reset_log()
-    al.configure_log_settings(log_etl_stage_time=False, log_etl_stage_rows=False)
-    al.log_etl_stage_time('cleaning', start_time, end_time)
-    al.log_etl_stage_rows_df('cleaning', dataframe_pandas)
-    al.log_etl_stage_time('spatial_join', start_datetime, end_datetime)
-    al.log_etl_stage_rows_df('spatial_join', dataframe_geopandas)
+    al.log_rows_false()
+    al.log_etl_stage_rows_df(ETL_STAGE_CLEAN, dataframe_pandas)
 
-    assert al.log_dict['cleaning_delta_time'] is None
-    assert al.log_dict['cleaning_rows'] is None
-    assert al.log_dict['spatial_join_delta_time'] is None
-    assert al.log_dict['spatial_join_rows'] is None
+    assert al.log_dict[f'{ETL_STAGE_CLEAN}_rows'] is None
 
 
 def test_audit_log_etl_stage_time_raises_error():
@@ -70,7 +60,7 @@ def test_audit_log_etl_stage_rows_df_raises_error():
 def test_audit_log_etl_stage_rows_df_raises_type_error():
     with pytest.raises(TypeError):
         al = AuditLogger()
-        al.log_etl_stage_rows_df('cleaning', None)
+        al.log_etl_stage_rows_df(ETL_STAGE_CLEAN, None)
 
 
 def test_audit_log_etl_stage_rows_cursor_raises_name_error():
@@ -82,7 +72,7 @@ def test_audit_log_etl_stage_rows_cursor_raises_name_error():
 def test_audit_log_etl_stage_rows_cursor_raises_attribute_error():
     with pytest.raises(AttributeError):
         al = AuditLogger()
-        al.log_etl_stage_rows_cursor('cleaning', None)
+        al.log_etl_stage_rows_cursor(ETL_STAGE_CLEAN, None)
 
 
 def test_audit_log_file_raises_error():
@@ -107,7 +97,7 @@ def test_audit_log_file(file_path):
     assert al.log_dict['file_size'] == file_size
     assert al.log_dict['file_rows'] == file_rows
 
-    al.configure_log_settings(log_file_rows=False)
+    al.log_rows_false()
 
     al.reset_log()
     al.log_file(file_path)
