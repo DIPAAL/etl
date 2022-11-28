2f76940d3391ea2d3651388529bac99bf28d8996
Lau Josefsen <ljosef18@student.aau.dk>
2022-11-28 09:06:00 +0100

    Add indexes

--- etl/init/sql/31_fact_cell.sql
+++ etl/init/sql/31_fact_cell.sql
@@ -13,9 +13,9 @@ CREATE TABLE fact_cell (
     PRIMARY KEY (cell_x, cell_y, ship_id, ship_junk_id, entry_date_id, entry_time_id, exit_date_id, exit_time_id, direction_id, nav_status_id, trajectory_sub_id),
 
     sog float NOT NULL,
-    delta_heading float NOT NULL,
-    draught float NOT NULL,
-    delta_cog float NOT NULL,
+    delta_heading float,
+    draught float,
+    delta_cog float,
 
     FOREIGN KEY (cell_x, cell_y) REFERENCES dim_cell_50m(x,y),
     FOREIGN KEY (ship_id) REFERENCES dim_ship(ship_id),
@@ -28,3 +28,8 @@ CREATE TABLE fact_cell (
     FOREIGN KEY (nav_status_id) REFERENCES dim_nav_status(nav_status_id),
     FOREIGN KEY (entry_date_id, trajectory_sub_id) REFERENCES dim_trajectory(date_id, trajectory_sub_id)
 ) PARTITION BY RANGE (entry_date_id);
+
+-- index over entry date and time
+CREATE INDEX fact_cell_entry_date_time_idx ON fact_cell (entry_date_id, entry_time_id);
+-- index over exit date and time
+CREATE INDEX fact_cell_exit_date_time_idx ON fact_cell (exit_date_id, exit_time_id);
