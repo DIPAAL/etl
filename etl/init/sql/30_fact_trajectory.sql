1bbf65c956dac96176d7063e84005fae40a33c1d
LauJosefsen <ljosef18@student.aau.dk>
2022-12-02 19:24:59 +0000

    Update etl/init/sql/30_fact_trajectory.sql

--- etl/init/sql/30_fact_trajectory.sql
+++ etl/init/sql/30_fact_trajectory.sql
@@ -26,3 +26,5 @@ CREATE TABLE fact_trajectory (
     FOREIGN KEY (nav_status_id) REFERENCES dim_nav_status(nav_status_id),
     FOREIGN KEY (start_date_id, trajectory_sub_id) REFERENCES dim_trajectory(date_id, trajectory_sub_id)
 ) PARTITION BY RANGE (start_date_id);
+
+CREATE INDEX fact_trajectory_start_date_id_trajectory_sub_id_idx ON dim_trajecotry (start_date_id, trajectory_sub_id);
