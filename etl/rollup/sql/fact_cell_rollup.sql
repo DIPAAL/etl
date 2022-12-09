631241dbbd5981fcd9103256ae7842008949f7be
LauJosefsen <ljosef18@student.aau.dk>
2022-12-09 13:33:07 +0000

    Update fact_cell_rollup.sql

--- etl/rollup/sql/fact_cell_rollup.sql
+++ etl/rollup/sql/fact_cell_rollup.sql
@@ -18,7 +18,7 @@ SELECT
     nav_status_id,
     trajectory_sub_id,
     length(crossing) / GREATEST(durationSeconds, 1) * 1.94 sog, -- 1 m/s = 1.94 knots. Min 1 second to avoid division by zero
-    calculate_delta(SELECT ARRAY_AGG(LOWER(head)) FROM UNNEST(GETVALUES(heading)) as head) delta_heading,
+    calculate_delta((SELECT ARRAY_AGG(LOWER(head)) FROM UNNEST(GETVALUES(heading)) as head)) delta_heading,
     draught,
     delta_cog,
     stbox(cell_geom, period(startTime, endTime)) st_bounding_box
