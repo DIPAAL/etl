b73ce1cb27ff010b348d0c591dd0a4ac7ae525ec
Lau Josefsen <ljosef18@student.aau.dk>
2022-12-15 13:00:05 +0100

    Added SQL queries

--- benchmarks/garbage_queries/garbage_areas.sql
+++
@@ -1,7 +0,0 @@
-SELECT STBOX(
-    (SELECT geom FROM enc_cells ORDER BY random() LIMIT 1),
-    -- create a random period between 2022-01-01 and 2022-03-31
-    SELECT period(t.start_time, t.start_time+interval '10 days') FROM (
-        SELECT '2022-01-01T00:00:00Z'::timestamptz + random() * ('2022-03-31T00:00:00Z'::timestamptz - '2022-01-01T00:00:00Z'::timestamptz) AS start_time
-    ) AS t
-) AS box;
