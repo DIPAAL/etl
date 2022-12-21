e8f9b7a4aee9123233b41a95a3778773a2abf88d
Lau Josefsen <ljosef18@student.aau.dk>
2022-12-21 13:16:34 +0100

    fix query

--- benchmarks/clear_cache.sh
+++ benchmarks/clear_cache.sh
@@ -14,11 +14,11 @@ chmod 600 ~/.ssh/id_rsa
 #130.225.39.242 ais-pg-db-3
 #130.225.39.166 ais-pg-db-4
 #130.225.39.249 ais-pg-db-5
-ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.200 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
-ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.121 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
-ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.242 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
-ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.166 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
-ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.249 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
+#ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.200 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
+#ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.121 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
+#ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.242 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
+#ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.166 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
+#ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.249 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
 
 
 # Statefulsets to be restarted:
@@ -28,11 +28,11 @@ ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.249 'sudo bash -c "sync; ech
 #ais-citus-worker-3
 #ais-citus-worker-4
 
-ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.200 'microk8s kubectl rollout restart statefulset ais-citus-master ais-citus-worker-1 ais-citus-worker-2 ais-citus-worker-3 ais-citus-worker-4 && microk8s kubectl rollout status statefulset ais-citus-master ais-citus-worker-1 ais-citus-worker-2 ais-citus-worker-3 ais-citus-worker-4'
+ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.200 'sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches" && microk8s kubectl rollout restart statefulset ais-citus-master ais-citus-worker-1 && microk8s kubectl rollout status statefulset ais-citus-master ais-citus-worker-1'
+#ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.200 'microk8s kubectl rollout restart statefulset ais-citus-master ais-citus-worker-1 ais-citus-worker-2 ais-citus-worker-3 ais-citus-worker-4 && microk8s kubectl rollout status statefulset ais-citus-master ais-citus-worker-1 ais-citus-worker-2 ais-citus-worker-3 ais-citus-worker-4'
 # wait for all pods to be ready
-ssh -o "StrictHostKeyChecking=no" ubuntu@130.225.39.200 microk8s kubectl wait --for=condition=ready pod ais-citus-master-0 ais-citus-worker-1-0 ais-citus-worker-2-0 ais-citus-worker-3-0 ais-citus-worker-4-0
 # sleep 5 seconds to let it settle a bit.
-sleep 5
+sleep 2
 
 # Cache should be cold now.
 
