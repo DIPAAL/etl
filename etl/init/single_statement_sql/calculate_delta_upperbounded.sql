a63a4d4379a6b7f69a915d0fe0471d584f4d7c17
Wolf <aklitg13@student.aau.dk>
2023-02-02 14:00:45 +0100

    Found an edge case that did not work with the definition of 'calculate_delta_upperbounded', worked out new definition that works

--- etl/init/single_statement_sql/calculate_delta_upperbounded.sql
+++ etl/init/single_statement_sql/calculate_delta_upperbounded.sql
@@ -8,8 +8,9 @@ BEGIN
     delta := 0;
     FOR i IN 2..array_upper(doubles, 1) LOOP
         -- Try the differences on both sides of upper_bound to find the lowest as the delta
-        delta_1 := ABS((doubles[i] - doubles[i-1]) - upper_bound);
-        delta_2 := ABS((doubles[i-1] - doubles[i]) - upper_bound);
+        -- Absolute is inherent from using modulo
+        delta_1 := dpmod((doubles[i] - doubles[i-1] + upper_bound), upper_bound);
+        delta_2 := dpmod((doubles[i-1] - doubles[i] + upper_bound), upper_bound);
         IF delta_1 < delta_2
         THEN
             delta := delta + delta_1;
