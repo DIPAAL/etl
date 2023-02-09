8dfd643ff642f5bd3253caf4999d9aceab0eb2dd
Wolf <aklitg13@student.aau.dk>
2023-02-09 14:45:44 +0100

    Made my tests a more resilient to change in hardcoded parameters

--- tests/trajectory_construction_test.py
+++ tests/trajectory_construction_test.py
@@ -10,7 +10,7 @@ from etl.cleaning.clean_data import create_dirty_df_from_ais_csv
 from etl.trajectory.builder import build_from_geopandas, rebuild_to_geodataframe, _euclidian_dist, \
     _create_trajectory_db_df, _check_outlier, extract_date_smart_id, _extract_time_smart_id, _find_most_recurring, \
     POINTS_FOR_TRAJECTORY_THRESHOLD, _finalize_trajectory, _tfloat_from_dataframe, COORDINATE_REFERENCE_SYSTEM_METERS, \
-    _update_stopped_index, _constraint_time_difference
+    _update_stopped_index, _constraint_time_difference, STOPPED_KNOTS_THRESHOLD, POINT_TIME_DIFFERENCE_SPLIT_THRESHOLD
 from etl.constants import CVS_TIMESTAMP_FORMAT, LONGITUDE_COL, LATITUDE_COL, SOG_COL, TIMESTAMP_COL, T_LENGTH_COL
 from etl.constants import T_START_DATE_COL, T_START_TIME_COL, T_END_DATE_COL, T_END_TIME_COL, T_ETA_DATE_COL, \
     T_ETA_TIME_COL, T_INFER_STOPPED_COL, T_A_COL, T_B_COL, T_C_COL, T_D_COL, T_IMO_COL, T_ROT_COL, T_MMSI_COL, \
@@ -293,14 +293,14 @@ def test_time_diff_split_constraint(test_file, expected_stopped, expected_moving
 
 
 test_updated_stopped_data = [
-    (0.5, 0, 0, None),
-    (0.6, 0, 0, None),
-    (0.7, 10, 5, None),
-    (0.3, 0, None, 0),
-    (0.4, 0, 0, 0),
-    (0.3, 5, None, 5),
-    (0.3, 0, None, 0),
-    (0.2, 10, 3, 3)
+    (STOPPED_KNOTS_THRESHOLD, 0, 0, None),
+    (STOPPED_KNOTS_THRESHOLD + 0.1, 0, 0, None),
+    (STOPPED_KNOTS_THRESHOLD + 0.2, 10, 5, None),
+    (STOPPED_KNOTS_THRESHOLD - 0.2, 0, None, 0),
+    (STOPPED_KNOTS_THRESHOLD - 0.1, 0, 0, 0),
+    (STOPPED_KNOTS_THRESHOLD - 0.2, 5, None, 5),
+    (STOPPED_KNOTS_THRESHOLD - 0.2, 0, None, 0),
+    (STOPPED_KNOTS_THRESHOLD - 0.3, 10, 3, 3)
 ]
 
 
@@ -311,31 +311,24 @@ def test_updated_stopped_index(sog: float, idx: int, stp_idx: int, expected_stp_
     assert expected_stp_idx == result
 
 
-def create_series(values):
-    return pd.Series(data=values)
-
-
 test_constraint_time_difference_data = [
-    (create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:10:10', format='%d/%m/%Y %H:%M:%S')}),
-        None, False),
-    (create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:10:10', format='%d/%m/%Y %H:%M:%S')}),
-        create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:10:10', format='%d/%m/%Y %H:%M:%S')}),
-        False),
-    (create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:25:09', format='%d/%m/%Y %H:%M:%S')}),
-        create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:10:10', format='%d/%m/%Y %H:%M:%S')}),
-        False),
-    (create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:25:10', format='%d/%m/%Y %H:%M:%S')}),
-        create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:10:10', format='%d/%m/%Y %H:%M:%S')}),
-        True),
-    (create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:25:11', format='%d/%m/%Y %H:%M:%S')}),
-        create_series({TIMESTAMP_COL: pd.to_datetime('10/10/2010 10:10:10', format='%d/%m/%Y %H:%M:%S')}),
-        True),
-    (None, None, False)
+    ('10/10/2010 10:10:10', None, False),
+    ('10/10/2010 10:10:10', 0, False),
+    ('10/10/2010 10:10:10', POINT_TIME_DIFFERENCE_SPLIT_THRESHOLD - 1, False),
+    ('10/10/2010 10:10:10', POINT_TIME_DIFFERENCE_SPLIT_THRESHOLD, True),
+    ('10/10/2010 10:10:10', POINT_TIME_DIFFERENCE_SPLIT_THRESHOLD + 1, True),
 ]
 
 
-@pytest.mark.parametrize('cur, prev, expected', test_constraint_time_difference_data)
-def test_constraint_time_difference(cur: gpd.GeoSeries, prev: gpd.GeoSeries, expected: bool):
+@pytest.mark.parametrize('time_str, seconds_to_add, expected', test_constraint_time_difference_data)
+def test_constraint_time_difference(time_str: str, seconds_to_add: int, expected: bool):
+    start_timestamp = datetime.strptime(time_str, '%d/%m/%Y %H:%M:%S')
+    prev = pd.Series(data={TIMESTAMP_COL: start_timestamp}) if seconds_to_add is not None else None
+
+    end_timestamp = start_timestamp + timedelta(seconds=seconds_to_add) \
+        if seconds_to_add is not None else start_timestamp
+    cur = pd.Series(data={TIMESTAMP_COL: end_timestamp})
+
     result = _constraint_time_difference(cur, prev)
 
     assert expected == result
