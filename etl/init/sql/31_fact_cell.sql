63bab06a0f15d5e21ff71062778bd5a55271ddbb
Mikael <mimikk21@student.aau.dk>
2023-02-10 10:00:56 +0100

    Corrected Comments

--- etl/init/sql/31_fact_cell.sql
+++ etl/init/sql/31_fact_cell.sql
@@ -1,24 +1,24 @@
 CREATE TABLE fact_cell (
     st_bounding_box stbox NOT NULL, -- 80 bytes
 
-    sog float NOT NULL, -- 8 bytes
+    sog float NOT NULL,
     delta_heading float,
     draught float,
     delta_cog float,
 
-    cell_x integer NOT NULL, -- 4 bytes
-    cell_y integer NOT NULL,
+    cell_x integer NOT NULL,
+    cell_y integer NOT NULL, -- Aligned
     ship_id integer NOT NULL,
-    entry_date_id integer NOT NULL,
+    entry_date_id integer NOT NULL, -- Aligned
     entry_time_id integer NOT NULL,
-    exit_date_id integer NOT NULL,
+    exit_date_id integer NOT NULL, -- Aligned
     exit_time_id integer NOT NULL,
-    trajectory_sub_id integer NOT NULL,
+    trajectory_sub_id integer NOT NULL, -- Aligned
     direction_id smallint NOT NULL, -- 2 bytes
     nav_status_id smallint NOT NULL,
-    -- Padding: 80 - (4 * 8 + 8 * 4 + 2 * 2) = 80 - 72 = 8 bytes
-    PRIMARY KEY (cell_x, cell_y, ship_id, entry_date_id, entry_time_id, exit_date_id, exit_time_id, direction_id, nav_status_id, trajectory_sub_id),
+    -- Padding: 2+2 = 4 bytes, so 4 bytes of padding to reach the MAXALIGN of 8 bytes
 
+    PRIMARY KEY (cell_x, cell_y, ship_id, entry_date_id, entry_time_id, exit_date_id, exit_time_id, direction_id, nav_status_id, trajectory_sub_id),
 
     FOREIGN KEY (cell_x, cell_y) REFERENCES dim_cell_50m(x,y),
     FOREIGN KEY (ship_id) REFERENCES dim_ship(ship_id),
