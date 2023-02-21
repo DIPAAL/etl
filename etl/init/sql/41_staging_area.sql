fedb2f9d41e1761a896524e786000063d7adf974
Lau Josefsen <ljosef18@student.aau.dk>
2023-02-21 14:37:39 +0100

    Added staging area

--- etl/init/sql/41_staging_area.sql
+++ etl/init/sql/41_staging_area.sql
@@ -6,22 +6,22 @@ CREATE SCHEMA IF NOT EXISTS staging;
 -- UpperLeft = 3602375,3471675
 -- LowerRight = 4392275,3055475
 
-CREATE TABLE IF NOT EXISTS staging.cell_50m (
+CREATE TABLE IF NOT EXISTS staging.cell_1000m (
     x integer NOT NULL,
     y integer NOT NULL,
     geom geometry NOT NULL,
     PRIMARY KEY (x, y)
 );
 
-INSERT INTO staging.cell_50m (x, y, geom)
+INSERT INTO staging.cell_1000m (x, y, geom)
 SELECT
     i as x,
     j as y,
     geom
 FROM
-    ST_SquareGrid(50, ST_SetSRID(ST_MakeBox2D(ST_Point(3602375, 3055475), ST_Point(4392275, 3471675)), 3034)) AS geom;
+    ST_SquareGrid(1000, ST_SetSRID(ST_MakeBox2D(ST_Point(3602375, 3055475), ST_Point(4392275, 3471675)), 3034)) AS geom;
 
-CREATE INDEX IF NOT EXISTS cell_50m_geom_idx ON staging.cell_50m USING SPGIST (geom);
+CREATE INDEX IF NOT EXISTS cell_1000m_geom_idx ON staging.cell_1000m USING SPGIST (geom);
 
 CREATE TABLE IF NOT EXISTS staging.cell_200m (
     x integer NOT NULL,
@@ -40,20 +40,25 @@ FROM
 
 CREATE INDEX IF NOT EXISTS cell_200m_geom_idx ON staging.cell_200m USING SPGIST (geom);
 
-CREATE TABLE IF NOT EXISTS staging.cell_1000m (
+CREATE TABLE IF NOT EXISTS staging.cell_50m (
     x integer NOT NULL,
     y integer NOT NULL,
     geom geometry NOT NULL,
     PRIMARY KEY (x, y)
 );
 
-INSERT INTO staging.cell_1000m (x, y, geom)
+INSERT INTO staging.cell_50m (x, y, geom)
 SELECT
     i as x,
     j as y,
     geom
 FROM
-    ST_SquareGrid(1000, ST_SetSRID(ST_MakeBox2D(ST_Point(3602375, 3055475), ST_Point(4392275, 3471675)), 3034)) AS geom;
+    ST_SquareGrid(50, ST_SetSRID(ST_MakeBox2D(ST_Point(3602375, 3055475), ST_Point(4392275, 3471675)), 3034)) AS geom;
+
+CREATE INDEX IF NOT EXISTS cell_50m_geom_idx ON staging.cell_50m USING SPGIST (geom);
+
+
+
 
 
 -- Make them all reference tables
