6fe3fe623b7b1605ffbb67b33bf5e9b9e41f0018
Wolf <aklitg13@student.aau.dk>
2023-02-23 13:37:32 +0100

    Created fact heatmap tables

--- etl/init/sql/32_fact_cell_heatmap.sql
+++ etl/init/sql/32_fact_cell_heatmap.sql
@@ -1,4 +1,44 @@
+-- Create 50m pre-aggregated heatmap table
 CREATE TABLE IF NOT EXISTS fact_cell_heatmap_50m (
+    cell_x INTEGER NOT NULL,
+    cell_y INTEGER NOT NULL,
+    date_id INTEGER NOT NULL,
+    ship_type_id INTEGER NOT NULL,
+    density_histogram INTEGER[24] NOT NULL,
+    PRIMARY KEY (cell_x, cell_y, date_id, ship_type_id),
+    FOREIGN KEY (cell_x, cell_y) REFERENCES dim_cell_50m(x, y),
+    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    FOREIGN KEY (ship_type_id) REFERENCES dim_ship_type(ship_type_id)
+);
+
+-- Create 200m pre-aggregated heatmap table
+CREATE TABLE IF NOT EXISTS fact_cell_heatmap_200m (
+    cell_x INTEGER NOT NULL,
+    cell_y INTEGER NOT NULL,
+    date_id INTEGER NOT NULL,
+    ship_type_id INTEGER NOT NULL,
+    density_histogram INTEGER[24] NOT NULL,
+    PRIMARY KEY (cell_x, cell_y, date_id, ship_type_id),
+    FOREIGN KEY (cell_x, cell_y) REFERENCES dim_cell_50m(x, y),
+    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    FOREIGN KEY (ship_type_id) REFERENCES dim_ship_type(ship_type_id)
+);
+
+-- Create 1000m pre-aggregated heatmap table
+CREATE TABLE IF NOT EXISTS fact_cell_heatmap_1000m (
+    cell_x INTEGER NOT NULL,
+    cell_y INTEGER NOT NULL,
+    date_id INTEGER NOT NULL,
+    ship_type_id INTEGER NOT NULL,
+    density_histogram INTEGER[24] NOT NULL,
+    PRIMARY KEY (cell_x, cell_y, date_id, ship_type_id),
+    FOREIGN KEY (cell_x, cell_y) REFERENCES dim_cell_50m(x, y),
+    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    FOREIGN KEY (ship_type_id) REFERENCES dim_ship_type(ship_type_id)
+);
+
+-- Create 5000m pre-aggregated heatmap table
+CREATE TABLE IF NOT EXISTS fact_cell_heatmap_5000m (
     cell_x INTEGER NOT NULL,
     cell_y INTEGER NOT NULL,
     date_id INTEGER NOT NULL,
