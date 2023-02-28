8c599d542d5c7321067edfb6c20dbfa5f7ce1f27
Wolf <aklitg13@student.aau.dk>
2023-02-28 13:08:03 +0100

    Fixed 'spaces' issue in SQL files and renamed an sql file to follow convention

--- etl/init/single_statement_sql/create_histogram.sql
+++ etl/init/single_statement_sql/create_histogram.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION create_histogram(rast RASTER, spatial_resolution INTE
 DECLARE
     histogram_id INTEGER;
 BEGIN
-	INSERT INTO dim_histogram AS dh (histogram, spatial_resolution, temporal_resolution) VALUES (rast, spatial_resolution, temporal_resolution) RETURNING dh.histogram_id INTO histogram_id;
+    INSERT INTO dim_histogram AS dh (histogram, spatial_resolution, temporal_resolution) VALUES (rast, spatial_resolution, temporal_resolution) RETURNING dh.histogram_id INTO histogram_id;
     RETURN histogram_id;
 END;
 $$ LANGUAGE plpgsql
--- etl/rollup/sql/heatmap.sql
+++ etl/rollup/sql/heatmap.sql
@@ -10,45 +10,45 @@ SELECT
     (i2.hour_of_day || '0000')::int AS time_id,
     i2.ship_type_id,
     (
-		SELECT
-        	create_histogram(
-            	i2.rast,
-            	{CELL_SIZE},
-            	3600
-        	)
+        SELECT
+            create_histogram(
+                i2.rast,
+                {CELL_SIZE},
+                3600
+            )
     ) AS histogram,
     (SELECT heatmap_type_id FROM dim_heatmap_type WHERE name = 'count') AS heatmap_type_id
 FROM
     (
-		SELECT
-			ST_Union(
-				ST_AsRaster(
-					i1.geom,
-				    rr.rast,
-					'32BUI'::text,
-					cnt::int
-				)
-			) AS rast,
-			i1.cell_x / (5000 / {CELL_SIZE}) AS cell_x,
-			i1.cell_y / (5000 / {CELL_SIZE}) AS cell_y,
-			i1.hour_of_day,
-			i1.ship_type_id
-		FROM raster_ref rr,
-		(
-			SELECT
-            	cell_x,
-            	cell_y,
-            	dt.hour_of_day,
-            	ds.ship_type_id,
-				dc.geom,
-				COUNT(*) cnt
-        	FROM fact_cell_{CELL_SIZE}m fc
-        	INNER JOIN dim_time dt ON dt.time_id = fc.entry_time_id
-        	INNER JOIN dim_ship ds ON ds.ship_id = fc.ship_id
-			INNER JOIN dim_cell_{CELL_SIZE}m dc ON dc.x = fc.cell_x and dc.y = fc.cell_y
-        	WHERE fc.entry_date_id = %(DATE_KEY)s
-        	GROUP BY fc.cell_x, fc.cell_y, dt.hour_of_day, ds.ship_type_id, dc.geom
-    	) i1
-		GROUP BY i1.cell_x / (5000 / {CELL_SIZE}), i1.cell_y / (5000 / {CELL_SIZE}), i1.hour_of_day, i1.ship_type_id
-	) i2
+        SELECT
+            ST_Union(
+                ST_AsRaster(
+                    i1.geom,
+                    rr.rast,
+                    '32BUI'::text,
+                    cnt::int
+                )
+            ) AS rast,
+            i1.cell_x / (5000 / {CELL_SIZE}) AS cell_x,
+            i1.cell_y / (5000 / {CELL_SIZE}) AS cell_y,
+            i1.hour_of_day,
+            i1.ship_type_id
+        FROM raster_ref rr,
+        (
+            SELECT
+                cell_x,
+                cell_y,
+                dt.hour_of_day,
+                ds.ship_type_id,
+                dc.geom,
+                COUNT(*) cnt
+            FROM fact_cell_{CELL_SIZE}m fc
+            INNER JOIN dim_time dt ON dt.time_id = fc.entry_time_id
+            INNER JOIN dim_ship ds ON ds.ship_id = fc.ship_id
+            INNER JOIN dim_cell_{CELL_SIZE}m dc ON dc.x = fc.cell_x and dc.y = fc.cell_y
+            WHERE fc.entry_date_id = %(DATE_KEY)s
+            GROUP BY fc.cell_x, fc.cell_y, dt.hour_of_day, ds.ship_type_id, dc.geom
+        ) i1
+        GROUP BY i1.cell_x / (5000 / {CELL_SIZE}), i1.cell_y / (5000 / {CELL_SIZE}), i1.hour_of_day, i1.ship_type_id
+    ) i2
 ;
