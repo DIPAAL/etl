ee5886f22508dfde8c7b96fd08bee83f696b5580
Wolf <aklitg13@student.aau.dk>
2023-02-28 13:15:27 +0100

    Removed 'unknown' heatmap type

--- etl/init/sql/19_heatmap_type.sql
+++ etl/init/sql/19_heatmap_type.sql
@@ -3,5 +3,4 @@ CREATE TABLE IF NOT EXISTS dim_heatmap_type (
     name text NOT NULL
 );
 
-INSERT INTO dim_heatmap_type (name) VALUES ('unknown');
 INSERT INTO dim_heatmap_type (name) VALUES ('count');
