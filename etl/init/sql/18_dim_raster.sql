2db167e4355efe6df4064231fcbc83eb96ff1d70
Wolf <aklitg13@student.aau.dk>
2023-03-01 10:30:41 +0100

    Changed naming scheme in 'dim_raster' to be based on SRID

--- etl/init/sql/18_dim_raster.sql
+++ etl/init/sql/18_dim_raster.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS dim_raster (
     raster_id SERIAL PRIMARY KEY,
     temporal_resolution_sec INTEGER NOT NULL,
-    spatial_resolution_meter INTEGER NOT NULL,
+    spatial_resolution_srid_unit INTEGER NOT NULL,
     rast raster NOT NULL
 );
