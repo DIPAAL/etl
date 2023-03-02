3ff648bd12a5216f310a4e1e9dd71a59ed4d239e
Wolf <aklitg13@student.aau.dk>
2023-03-02 14:48:25 +0100

    Removed time grouping from heatmap generation

--- etl/rollup/sql/heatmap.sql
+++ etl/rollup/sql/heatmap.sql
@@ -4,7 +4,7 @@ SELECT
     i2.cell_x,
     i2.cell_y,
     %(DATE_KEY)s AS date_id,
-    (i2.hour_of_day || '0000')::int AS time_id,
+    0 AS time_id,
     i2.ship_type_id,
     (SELECT insert_raster(i2.rast)) AS raster_id,
     (SELECT heatmap_type_id FROM dim_heatmap_type WHERE name = 'count') AS heatmap_type_id,
@@ -23,23 +23,20 @@ FROM
             ) AS rast,
             i1.cell_x / (5000 / {CELL_SIZE}) AS cell_x,
             i1.cell_y / (5000 / {CELL_SIZE}) AS cell_y,
-            i1.hour_of_day,
             i1.ship_type_id
         FROM
         (
             SELECT
                 cell_x,
                 cell_y,
-                dt.hour_of_day,
                 ds.ship_type_id,
                 fc.st_bounding_box::geometry AS geom,
                 COUNT(*) cnt
             FROM fact_cell_{CELL_SIZE}m fc
-            INNER JOIN dim_time dt ON dt.time_id = fc.entry_time_id
             INNER JOIN dim_ship ds ON ds.ship_id = fc.ship_id
             WHERE fc.entry_date_id = %(DATE_KEY)s
-            GROUP BY fc.cell_x, fc.cell_y, dt.hour_of_day, ds.ship_type_id, fc.st_bounding_box::geometry
+            GROUP BY fc.cell_x, fc.cell_y, ds.ship_type_id, fc.st_bounding_box::geometry
         ) i1
-        GROUP BY i1.cell_x / (5000 / {CELL_SIZE}), i1.cell_y / (5000 / {CELL_SIZE}), i1.hour_of_day, i1.ship_type_id
+        GROUP BY i1.cell_x / (5000 / {CELL_SIZE}), i1.cell_y / (5000 / {CELL_SIZE}), i1.ship_type_id
     ) i2
 ;
