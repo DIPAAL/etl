544373a4f1d037931e51867c6c5cb24dc2d0af3c
Wolf <aklitg13@student.aau.dk>
2023-03-03 15:25:14 +0100

    Fixed error with lower-level cell sizes not references not being enforced

--- etl/init/sql/14_dim_cell.sql
+++ etl/init/sql/14_dim_cell.sql
@@ -14,8 +14,8 @@ CREATE INDEX dim_cell_5000m_geom_idx ON dim_cell_5000m USING spgist (geom);
 CREATE TABLE dim_cell_1000m (
     x integer NOT NULL,
     y integer NOT NULL,
-    parent_x integer,
-    parent_y integer,
+    parent_x integer NOT NULL,
+    parent_y integer NOT NULL,
     geom geometry NOT NULL,
     PRIMARY KEY (x, y),
     FOREIGN KEY (parent_x, parent_y) REFERENCES dim_cell_5000m(x,y)
@@ -27,8 +27,8 @@ CREATE INDEX dim_cell_1000m_geom_idx ON dim_cell_1000m USING spgist (geom);
 CREATE TABLE dim_cell_200m (
     x integer NOT NULL,
     y integer NOT NULL,
-    parent_x integer,
-    parent_y integer,
+    parent_x integer NOT NULL,
+    parent_y integer NOT NULL,
     geom geometry NOT NULL,
     PRIMARY KEY (x, y),
     FOREIGN KEY (parent_x, parent_y) REFERENCES dim_cell_1000m(x,y)
@@ -40,8 +40,8 @@ CREATE INDEX dim_cell_200m_geom_idx ON dim_cell_200m USING spgist (geom);
 CREATE TABLE dim_cell_50m (
     x integer NOT NULL,
     y integer NOT NULL,
-    parent_x integer,
-    parent_y integer,
+    parent_x integer NOT NULL,
+    parent_y integer NOT NULL,
     geom geometry NOT NULL,
     PRIMARY KEY (x, y),
     FOREIGN KEY (parent_x, parent_y) REFERENCES dim_cell_200m(x,y)
