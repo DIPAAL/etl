02cba81e646f879f2a9d3c838b47778c8304a3f3
Mikael <mimikk21@student.aau.dk>
2023-03-06 13:32:51 +0100

    Changed rollup

--- etl/rollup/sql/fact_cell_rollup.sql
+++ etl/rollup/sql/fact_cell_rollup.sql
@@ -41,7 +41,7 @@ FROM (
         nav_status_id,
         infer_stopped,
         trajectory_sub_id,
-        startValue (atPeriod (draught, crossing_period)) draught,
+        minValue(draught) draught,
         atPeriod (heading, crossing_period) heading,
         startTime,
         endTime,
