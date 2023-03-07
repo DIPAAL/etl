40d7a615ffa27376a08a96a48dcf5754ee2a1fe0
Wolf <aklitg13@student.aau.dk>
2023-03-07 13:34:37 +0100

    Added 'partition_id' to enable co-located rollup

--- etl/rollup/sql/lazy_load_cells_from_cell_facts.sql
+++ etl/rollup/sql/lazy_load_cells_from_cell_facts.sql
@@ -3,8 +3,8 @@ WITH rows AS (
     SELECT cell_x, cell_y, {PARENT_FORMULA_X}, {PARENT_FORMULA_Y}, st_bounding_box::geometry, partition_id
     FROM fact_cell_{CELL_SIZE}m
     WHERE entry_date_id = %s
-    GROUP BY cell_x, cell_y, st_bounding_box::geometry
-    ON CONFLICT (x, y) DO NOTHING
+    GROUP BY partition_id, cell_x, cell_y, st_bounding_box::geometry
+    ON CONFLICT (x, y, partition_id) DO NOTHING
     RETURNING 1
 )
 SELECT count(*) FROM rows;
--- etl/rollup/sql/staging_split_trajectories.sql
+++ etl/rollup/sql/staging_split_trajectories.sql
@@ -2,20 +2,26 @@ TRUNCATE staging.split_trajectories;
 
 INSERT INTO staging.split_trajectories
 SELECT
-    t.trajectory_sub_id,
-    t.ship_id,
-    t.nav_status_id,
-    t.infer_stopped,
-    (t.split).point point,
-    -- This forces lower and upper bound inclusiveness in the trajectory segments created by spaceSplit and sets linear interpolation between the points (lower, upper, linear)
-    tgeompoint_seq(INSTANTS(ROUND(UNNEST(sequences((t.split).tpoint)), 3)), true, true, true) AS trajectory, -- round to 1 milimeter, to avoid floating point errors
-    t.heading,
-    t.draught
+    t2.*,
+    (SELECT partition_id FROM spatial_partition sp WHERE ST_Contains(sp.geom, t2.trajectory::geometry)) AS partition_id
 FROM (
     SELECT
-        ft.trajectory_sub_id, ft.ship_id, ft.nav_status_id, ft.infer_stopped,
-        spaceSplit(transform(dt.trajectory, 3034), 5000) split,
-        dt.heading heading, dt.draught draught
-    FROM fact_trajectory ft
-    JOIN dim_trajectory dt ON ft.trajectory_sub_id = dt.trajectory_sub_id AND ft.start_date_id = dt.date_id
-    WHERE ft.start_date_id = %s) t;
+        t.trajectory_sub_id,
+        t.ship_id,
+        t.nav_status_id,
+        t.infer_stopped,
+        (t.split).point point,
+        -- This forces lower and upper bound inclusiveness in the trajectory segments created by spaceSplit and sets linear interpolation between the points (lower, upper, linear)
+        tgeompoint_seq(INSTANTS(ROUND(UNNEST(sequences((t.split).tpoint)), 3)), true, true, true) AS trajectory, -- round to 1 milimeter, to avoid floating point errors
+        t.heading,
+        t.draught
+    FROM (
+        SELECT
+            ft.trajectory_sub_id, ft.ship_id, ft.nav_status_id, ft.infer_stopped,
+            spaceSplit(transform(dt.trajectory, 3034), 5000) split,
+            dt.heading heading, dt.draught draught
+        FROM fact_trajectory ft
+        JOIN dim_trajectory dt ON ft.trajectory_sub_id = dt.trajectory_sub_id AND ft.start_date_id = dt.date_id
+        WHERE ft.start_date_id = %s) t
+    ) t2
+;
