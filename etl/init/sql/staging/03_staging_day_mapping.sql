5ca88e548272f79590a57b1e930848430fe33283
Wolf <aklitg13@student.aau.dk>
2023-03-10 10:22:02 +0100

    Fixed minor brainfart in the semantics of 'weekday' dimension attribute values

--- etl/init/sql/staging/03_staging_day_mapping.sql
+++ etl/init/sql/staging/03_staging_day_mapping.sql
@@ -7,12 +7,13 @@ CREATE TABLE IF NOT EXISTS day_num_map (
 );
 
 INSERT INTO day_num_map (day_num, day_name, weekday) VALUES
-    (0, 'sunday', 'no'),
-    (1, 'monday', 'yes'),
-    (2, 'tuesday', 'yes'),
-    (3, 'wednesday', 'yes'),
-    (4, 'thursday', 'yes'),
-    (5, 'friday', 'yes'),
-    (6, 'saturday', 'no');
+    (0, 'sunday', 'weekend'),
+    (1, 'monday', 'weekday'),
+    (2, 'tuesday', 'weekday'),
+    (3, 'wednesday', 'weekday'),
+    (4, 'thursday', 'weekday'),
+    (5, 'friday', 'weekday'),
+    (6, 'saturday', 'weekend'),
+    (7, 'sunday', 'weekend'); -- To also support 'ISODOW'
 
 SELECT create_reference_table('day_num_map');
