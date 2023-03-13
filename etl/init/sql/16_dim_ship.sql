6a157c5aa3a27a077126c67366086af683f6882e
Wolf <aklitg13@student.aau.dk>
2023-03-13 13:16:20 +0100

    Updated padding information for 'dim_ship'

--- etl/init/sql/16_dim_ship.sql
+++ etl/init/sql/16_dim_ship.sql
@@ -16,7 +16,7 @@ CREATE TABLE dim_ship (
     flag_state TEXT NOT NULL,
     UNIQUE (imo, mmsi, name, callsign, a, b, c, d, ship_type_id, location_system_type),
     FOREIGN KEY (ship_type_id) REFERENCES dim_ship_type(ship_type_id)
-    -- Padding: Maybe, at worst 12 bytes. Text attributes are variable length and will round up to nearest 4 bytes.
+    -- Padding: Undeterminable as "name" and "callsign" can be whatever they want to enter and an upper bound is therefore not possible to establish.
 );
 
 -- Create index on mmsi
