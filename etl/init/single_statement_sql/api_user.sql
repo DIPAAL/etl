da401ebdc8591b7b1a9d72914807d4423a5807bf
Lau Josefsen <ljosef18@student.aau.dk>
2023-03-16 13:45:42 +0100

    Added api user

---
+++ etl/init/single_statement_sql/api_user.sql
@@ -0,0 +1,18 @@
+do
+$body$
+declare
+  num_users integer;
+begin
+   SELECT count(*)
+     into num_users
+   FROM pg_user
+   WHERE usename = 'api'; -- No this is not a typo.
+
+   IF num_users = 0 THEN
+      CREATE ROLE api;
+   END IF;
+end
+$body$
+;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO api;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO api;
