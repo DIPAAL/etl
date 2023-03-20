c8c48a26e5e0cac2306c25e0c355cafec606ad66
Wolf <aklitg13@student.aau.dk>
2023-03-20 13:13:03 +0100

    Removed unsued files

--- etl/init/single_statement_sql/04_heatmap1000m_procedure_distribution.sql
+++
@@ -1,4 +0,0 @@
-SELECT create_distributed_function(
-    'create_heatmap_1000m(INTEGER,INTEGER,INTEGER,SMALLINT)',
-    'part_id', colocate_with := 'fact_cell_1000m'
-);
--- etl/init/single_statement_sql/06_heatmap200m_procedure_distribution.sql
+++
@@ -1,4 +0,0 @@
-SELECT create_distributed_function(
-    'create_heatmap_200m(INTEGER,INTEGER,INTEGER,SMALLINT)',
-    'part_id', colocate_with := 'fact_cell_200m'
-);
