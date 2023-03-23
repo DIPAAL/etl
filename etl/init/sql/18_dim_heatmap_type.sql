cbdf1b0495641da9f02be9f1f3dd9994a78e9461
Lau Josefsen <ljosef18@student.aau.dk>
2023-03-23 11:11:44 +0100

    Fixes for heatmaps

--- etl/init/sql/18_dim_heatmap_type.sql
+++ etl/init/sql/18_dim_heatmap_type.sql
@@ -1,6 +1,13 @@
 CREATE TABLE IF NOT EXISTS dim_heatmap_type (
     heatmap_type_id SMALLSERIAL PRIMARY KEY,
-    name text NOT NULL
+    slug text NOT NULL UNIQUE,
+    name text NOT NULL,
+    description text
 );
 
-INSERT INTO dim_heatmap_type (name) VALUES ('count');
+INSERT INTO dim_heatmap_type (slug, name, description) VALUES
+(
+    'count',
+    'Count',
+    'Count of trajectories crossing the cell'
+);
