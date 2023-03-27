3f7c307448a8f434bb95c4ae7ba07c954c5d06f5
Lau Josefsen <ljosef18@student.aau.dk>
2023-03-27 09:53:47 +0200

    Should fix cell fact rollups wher splitted trajectories did not get a partition id

--- etl/rollup/sql/staging_split_trajectories.sql
+++ etl/rollup/sql/staging_split_trajectories.sql
@@ -15,18 +15,19 @@ FROM (
         tgeompoint_seq(INSTANTS(ROUND(UNNEST(sequences((t.split).tpoint)), 3)), 'linear', true, true) AS trajectory, -- round to 1 milimeter, to avoid floating point errors
         t.heading,
         t.draught
-    FROM
-         (SELECT ft.trajectory_sub_id,
-                 ft.ship_id,
-                 ft.nav_status_id,
-                 ft.infer_stopped,
-                 spaceSplit(transform(dt.trajectory, 3034), 5000) split,
-                 dt.heading                                       heading,
-                 dt.draught                                       draught
-          FROM fact_trajectory ft
-                   JOIN dim_trajectory dt
-                        ON ft.trajectory_sub_id = dt.trajectory_sub_id AND ft.start_date_id = dt.date_id
-          WHERE ft.start_date_id = %s) t
-         ) t2
+    FROM (
+        SELECT
+            ft.trajectory_sub_id,
+            ft.ship_id,
+            ft.nav_status_id,
+            ft.infer_stopped,
+            spaceSplit(transform(dt.trajectory, 3034), 5000) split,
+            dt.heading heading,
+            dt.draught draught
+        FROM fact_trajectory ft
+        JOIN dim_trajectory dt ON ft.trajectory_sub_id = dt.trajectory_sub_id AND ft.start_date_id = dt.date_id
+        WHERE ft.start_date_id = % s
+    ) t
+) t2
 LEFT JOIN spatial_partition sp ON ST_Contains(sp.geom, t2.trajectory::geometry)
 ;
