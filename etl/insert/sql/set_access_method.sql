a2ee7684c26a712a404411e058c7cf3568f30579
Wolf <aklitg13@student.aau.dk>
2023-03-28 11:38:32 +0200

    Implemented end of month restribing

--- etl/insert/sql/set_access_method.sql
+++ etl/insert/sql/set_access_method.sql
@@ -1,2 +1,2 @@
-SET LOCAL citus.multi_shard_modify_mode TO 'sequential';
+SET LOCAL citus.multi_shard_modify_mode TO 'parallel';
 SELECT alter_table_set_access_method('{TABLE_NAME}', '{ACCESS_METHOD}');
