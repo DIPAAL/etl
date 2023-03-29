8218715197703ba7a70f23ce0becff2dd0ec0525
Wolf <aklitg13@student.aau.dk>
2023-03-29 13:03:49 +0200

    Added a bit of comments to the cleanup functions

--- etl/init/single_statement_sql/98_rollback.sql
+++ etl/init/single_statement_sql/98_rollback.sql
@@ -10,6 +10,7 @@ BEGIN
     DELETE FROM fact_cell_200m WHERE entry_date_id = ANY(dateid);
     DELETE FROM fact_cell_50m WHERE entry_date_id = ANY(dateid);
 
+    -- Determine the partition name based on first entry in array
     partition_name := 'fact_cell_heatmap_' || substring(dateid[1]::text FROM 1 FOR 4) || '_' || substring(dateid[1]::text FROM 5 FOR 2);
     RAISE NOTICE 'Start converting % to row-major storage scheme', partition_name;
     PERFORM alter_table_set_access_method(partition_name, 'heap');
--- etl/init/single_statement_sql/99_rollback_multi.sql
+++ etl/init/single_statement_sql/99_rollback_multi.sql
@@ -7,9 +7,11 @@ DECLARE
     dateid_arr INTEGER[];
     cnt SMALLINT;
 BEGIN
+    -- Get timestamp versions of the start and end dates, timeid = 0 because it is not used
     start_timestamp := timestamp_from_date_time_id(from_dateid, 0);
     end_timestamp := timestamp_from_date_time_id(to_dateid, 0);
 
+    -- Set initial values
     cur_month := 0;
     dateid_arr := ARRAY[]::INTEGER[];
     cnt := 0;
@@ -23,6 +25,7 @@ BEGIN
         IF cur_month = 0 THEN
             cur_month = datetime.month;
         END IF;
+        -- If we reach batch size or change month we clean and start batching again
         IF cnt >= max_batch_size OR cur_month != datetime.month THEN
             PERFORM dipaal_rollback(dateid_arr);
             cur_month := datetime.month;
@@ -33,6 +36,7 @@ BEGIN
             cnt := cnt + 1;
         END IF;
     END LOOP;
+    -- If we have some left, we clean the rest
     IF array_upper(dateid_arr, 1) != 0 THEN
         PERFORM dipaal_rollback(dateid_arr);
     END IF;
