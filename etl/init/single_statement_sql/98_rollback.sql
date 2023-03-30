eb1ad036dd139b6e05d904a74bdce208da56c845
Wolf <aklitg13@student.aau.dk>
2023-03-30 12:05:58 +0200

    Added error raising if function used incorretly

--- etl/init/single_statement_sql/98_rollback.sql
+++ etl/init/single_statement_sql/98_rollback.sql
@@ -1,7 +1,18 @@
 CREATE OR REPLACE FUNCTION dipaal_rollback(dateid INTEGER[]) RETURNS void AS $$
 DECLARE
     partition_name TEXT;
+    month INTEGER;
 BEGIN
+    month := 0;
+    FOR id IN array_lower(dateid, 1)..array_upper(dateid, 1)
+    LOOP
+        IF month = 0 THEN
+            month := substring(dateid[id]::text FROM 5 FOR 2)::INTEGER;
+        ELSIF month != substring(dateid[id]::text FROM 5 FOR 2)::INTEGER THEN
+            RAISE EXCEPTION 'Dateids provided do not belong to the same month, which is not allowed for this function';
+        END IF;
+    END LOOP;
+
     DELETE FROM fact_trajectory WHERE start_date_id = ANY(dateid);
     DELETE FROM dim_trajectory WHERE date_id = ANY(dateid);
 
