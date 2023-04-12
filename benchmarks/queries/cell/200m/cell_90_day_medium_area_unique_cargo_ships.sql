702c123cb5122cc95aa90f2feed4fb2b61424cdb
Wolf <aklitg13@student.aau.dk>
2023-04-12 11:09:31 +0200

    Updated existing queries to run on cell hierarchy

--- benchmarks/queries/cell/200m/cell_90_day_medium_area_unique_cargo_ships.sql
+++ benchmarks/queries/cell/200m/cell_90_day_medium_area_unique_cargo_ships.sql
@@ -4,16 +4,16 @@ WITH
         SELECT
             SetSRID(STBox(
                 enc.geom,
-                period('2022-01-01 00:00:00+00', '2022-04-01 00:00:00+00')
-            ),0) box,
+                span(timestamp_from_date_time_id(20220101, 0), timestamp_from_date_time_id(20220331, 0), true, false)
+            ), 3034) box,
             20220101 start_date_id,
             20220331 end_date_id
         FROM enc
     )
 SELECT distinct(ds.*)
 FROM q_window q
-INNER JOIN fact_cell fc ON fc.st_bounding_box && q.box
+INNER JOIN fact_cell_200m fc ON fc.st_bounding_box && q.box
 INNER JOIN dim_ship ds ON ds.ship_id = fc.ship_id
-INNER JOIN dim_ship_junk dsj ON dsj.ship_junk_id = fc.ship_junk_id
-WHERE fc.entry_date_id BETWEEN q.start_date_id AND q.end_date_id AND dsj.ship_type = 'Cargo'
+INNER JOIN dim_ship_type dst on ds.ship_type_id = dst.ship_type_id
+WHERE fc.entry_date_id BETWEEN q.start_date_id AND q.end_date_id AND dst.ship_type = 'Cargo'
 ;
--- benchmarks/queries/cell/trajectory/traj_1_day_large_area_unique_cargo_ships.sql
+++ benchmarks/queries/cell/trajectory/traj_1_day_large_area_unique_cargo_ships.sql
@@ -4,10 +4,10 @@ WITH
         SELECT
             STBox(
                 enc.geom_geodetic,
-                period('2022-01-26 00:00:00+00', '2022-02-25 00:00:00+00')
+                span(timestamp_from_date_time_id(20220110, 0), timestamp_from_date_time_id(20220110, 0), true, false)
             ) box,
-            20220126 start_date_id,
-            20220224 end_date_id
+            20220110 start_date_id,
+            20220110 end_date_id
         FROM enc
     )
 SELECT DISTINCT(ds.*)
@@ -15,6 +15,6 @@ FROM q_window
 INNER JOIN dim_trajectory dt ON atStBox(dt.trajectory, q_window.box) IS NOT NULL
 INNER JOIN fact_trajectory ft ON dt.date_id = ft.start_date_id AND dt.trajectory_sub_id = ft.trajectory_sub_id
 INNER JOIN dim_ship ds ON ds.ship_id = ft.ship_id
-INNER JOIN dim_ship_junk dsj ON dsj.ship_junk_id = ft.ship_junk_id
-WHERE ft.start_date_id BETWEEN q_window.start_date_id AND q_window.end_date_id AND dsj.ship_type = 'Cargo'
+INNER JOIN dim_ship_type dst on ds.ship_type_id = dst.ship_type_id
+WHERE ft.start_date_id BETWEEN q_window.start_date_id AND q_window.end_date_id AND dst.ship_type = 'Cargo'
 ;
