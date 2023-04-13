ade38426a84a3e218857fb9f6d196449abb3e4a9
Wolf <aklitg13@student.aau.dk>
2023-04-13 09:04:07 +0200

    Fixed small error with wrong fact_cell level being used for a query

--- benchmarks/queries/cell/50m/cell_90_day_single_partition_area_unique_cargo_ships.sql
+++ benchmarks/queries/cell/50m/cell_90_day_single_partition_area_unique_cargo_ships.sql
@@ -12,7 +12,7 @@ WITH
     )
 SELECT distinct(ds.*)
 FROM q_window q
-INNER JOIN fact_cell_200m fc ON fc.st_bounding_box && q.box
+INNER JOIN fact_cell_50m fc ON fc.st_bounding_box && q.box
 INNER JOIN dim_ship ds ON ds.ship_id = fc.ship_id
 INNER JOIN dim_ship_type dst on ds.ship_type_id = dst.ship_type_id
 WHERE fc.entry_date_id BETWEEN q.start_date_id AND q.end_date_id AND dst.ship_type = 'Cargo'
