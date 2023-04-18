5d218655f93cc0f649e8a1ddb42980ba310b1314
Wolf <aklitg13@student.aau.dk>
2023-04-18 15:26:53 +0200

    Split multiclass files into individual files

---
+++ benchmarks/configurations/cell_benchmark_configuration.py
@@ -0,0 +1,62 @@
+"""Module defining cell benchmark configurations."""
+from benchmarks.enumerations.cell_benchmark_configuration_type import CellBenchmarkConfigurationType
+from typing import List, Dict
+
+
+class CellBenchmarkConfiguration:
+    """Class defining how a cell benchmark should be run."""
+
+    VALID_RESOLUTIONS = [5000, 1000, 200, 50]
+
+    def __init__(self, start_date_id: int, end_date_id: int, enc_cell_id: int, ship_types: List[str],
+                 configuration_type: CellBenchmarkConfigurationType, spatial_resolution: int | None = None) -> None:
+        """
+        Initialize a cell benchmark configuration.
+
+        Arguments:
+            start_date_id: the data warehouse date smart ID for the start date used in benchmar queries
+            end_date_id: the data warehouse date smart ID for the end date used in benchmark queries
+            enc_cell_id: the ID of the enc cell determining the spatial area of the benchmark queries
+            ship_types: a list of ship types used to filter the benchmark queries
+            configuration_type: the type for the configuration
+            spatial_resolution: the spatial resolution for the configuration (default: None)
+        """
+        self.start_date_id = start_date_id
+        self.end_date_id = end_date_id
+        self.spatial_resolution = spatial_resolution
+        self.enc_cell_id = enc_cell_id
+        self.ship_types = ship_types
+        self.type = configuration_type
+        self.__validate_spatial_resolution(spatial_resolution)
+
+    def __validate_spatial_resolution(self, resolution: int) -> None:
+        """
+        Validate the spatial resolution of the configuration.
+
+        Arguments:
+            resolution: the given spatial resolution
+        """
+        if CellBenchmarkConfigurationType.CELL == self.type \
+                and resolution not in self.VALID_RESOLUTIONS:
+            raise ValueError(f'Invalid spatial resolution <{resolution}> provided for CellBenchmarkConfiguration. '
+                             'Only <{self.VALID_RESOLUTIONS}> are supported')
+
+    def get_parameters(self) -> Dict[str, any]:
+        """Return query parameters based on configuration."""
+        return {
+            'START_ID': self.start_date_id,
+            'END_ID': self.end_date_id,
+            'AREA_ID': self.enc_cell_id,
+            'SHIP_TYPES': self.ship_types
+        }
+
+    def format_query(self, query: str) -> str:
+        """
+        Format the query based on the configuration.
+
+        Arguments:
+            query: the query to format
+        """
+        if CellBenchmarkConfigurationType.CELL == self.type:
+            return query.format(CELL_SIZE=self.spatial_resolution)
+        return query
---
+++ benchmarks/runners/abstract_runtime_benchmark_runner.py
@@ -0,0 +1,46 @@
+"""Module containing the abstract runtime benchmark runner which all runtime benchmark runners inherit from."""
+import json
+from abc import ABC
+from sqlalchemy import text
+from benchmarks.runners.abstract_benchmark_runner import AbstractBenchmarkRunner
+from benchmarks.dataclasses.runtime_benchmark_result import RuntimeBenchmarkResult
+
+
+class AbstractRuntimeBenchmarkRunner(AbstractBenchmarkRunner, ABC):
+    """Abstract superclass that all runtime benchmark runners should inherit from."""
+
+    QUERY_NAME_KEY = 'name'
+    BENCHMARK_ID_KEY = 'id'
+
+    def __init__(self, garbage_queries_folder: str, garbage_queries_per_iteration: int = 10, iterations: int = 10) \
+            -> None:
+        """
+        Initialize an abstract runtime benchmark runner.
+
+        Arguments:
+            garbage_queries_folder: path to folder containing queries for prewarming the OS cache
+            garbage_queries_per_iteration: how many queries should be run to prewarm cache (default: 10)
+            iterations: how many times should each query in the benchmark be repeated (default: 10)
+        """
+        super().__init__(garbage_queries_folder, garbage_queries_per_iteration, iterations)
+
+    def _store_result(self, iteration: int, result: RuntimeBenchmarkResult) -> None:
+        """
+        Store the result of running the benchmark.
+
+        Arguments:
+            iteration: the iteration of the benchmark
+            result: all the results of running the particular benchmark
+        """
+        json_result = result.result.fetchone()[0][0]
+        data = json.dumps(json_result)
+
+        self._conn.execute(
+            text("""
+                INSERT INTO benchmark_results
+                (test_run_id, query_name, iteration, explain, execution_time_ms)
+                VALUES (:id, :name, :it, :result, :time)
+            """),
+            {'id': result.benchmark_id, 'name': result.benchmark_name,
+             'it': iteration, 'result': data, 'time': result.time_taken}
+        )
