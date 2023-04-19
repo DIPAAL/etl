a33fdef541dff96f63dc51f07b958870743b2eae
Wolf <aklitg13@student.aau.dk>
2023-04-19 11:15:39 +0200

    Added a bit more randomization to cell garbage queries

--- benchmarks/garbage_queries/cell/01_cell_garbage_query_10_day_random_enc.sql
+++ benchmarks/garbage_queries/cell/01_cell_garbage_query_10_day_random_enc.sql
@@ -17,5 +17,6 @@ FROM q_window q
 INNER JOIN fact_cell_5000m fc ON fc.st_bounding_box && q.box
 INNER JOIN dim_ship ds ON ds.ship_id = fc.ship_id
 INNER JOIN dim_ship_type dst ON dst.ship_type_id = ds.ship_type_id
-WHERE fc.entry_date_id BETWEEN 20220101 AND 20220331 AND dst.ship_type = 'Fishing'
+WHERE fc.entry_date_id BETWEEN 20220101 AND 20220331
+  dst.ship_type IN (SELECT ship_type FROM dim_ship_type ORDER BY random() LIMIT floor(random() * 8 + 2)::integer) -- random between 2 and 10
 ;
--- benchmarks/garbage_queries/cell/02_trajectory_garbage_query_10_day_random_enc.sql
+++ benchmarks/garbage_queries/cell/02_trajectory_garbage_query_10_day_random_enc.sql
@@ -18,5 +18,6 @@ INNER JOIN dim_trajectory dt ON atStBox(dt.trajectory, SetSRID(q_window.box,4326
 INNER JOIN fact_trajectory ft ON dt.date_id = ft.start_date_id AND dt.trajectory_sub_id = ft.trajectory_sub_id
 INNER JOIN dim_ship ds ON ds.ship_id = ft.ship_id
 INNER JOIN dim_ship_type dst ON dst.ship_type_id = ds.ship_type_id
-WHERE ft.start_date_id BETWEEN 20220101 AND 20220331 AND dst.ship_type = 'Fishing'
+WHERE ft.start_date_id BETWEEN 20220101 AND 20220331 
+  AND dst.ship_type IN (SELECT ship_type FROM dim_ship_type ORDER BY random() LIMIT floor(random() * 8 + 2)::integer) -- random between 2 and 10
 ;
