caaeaa637f2b9378a118637da5945ce2038c6ef3
Wolf <aklitg13@student.aau.dk>
2023-05-03 11:38:26 +0200

    Ensured both benchmark queries return the same amount of columns and in the same order

--- benchmarks/queries/dynamic_50m/50m_query.sql
+++ benchmarks/queries/dynamic_50m/50m_query.sql
@@ -1,5 +1,20 @@
 SELECT
-    *
+    fc.cell_x,
+    fc.cell_y,
+    fc.ship_id,
+    fc.entry_date_id,
+    fc.entry_time_id,
+    fc.exit_date_id,
+    fc.exit_time_id,
+    fc.direction_id,
+    fc.nav_status_id,
+    fc.infer_stopped,
+    fc.trajectory_sub_id,
+    fc.sog,
+    fc.delta_heading,
+    fc.draught,
+    fc.delta_cog,
+    fc.st_bounding_box
 FROM fact_cell_50m fc
 WHERE fc.st_bounding_box && stbox(
         st_makeenvelope(:xmin, :ymin, :xmax, :ymax, 3034),
