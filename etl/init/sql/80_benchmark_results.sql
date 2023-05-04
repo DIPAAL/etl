15ca24575037b9eb3e08fab5ea30fec676b64bb7
Wolf <aklitg13@student.aau.dk>
2023-05-04 14:12:57 +0200

    Re-arrenged for padding

--- etl/init/sql/80_benchmark_results.sql
+++ etl/init/sql/80_benchmark_results.sql
@@ -1,14 +1,14 @@
 -- Create table for benchmark results
 CREATE TABLE IF NOT EXISTS benchmark_results (
     id SERIAL PRIMARY KEY,
-    query_name VARCHAR(255) NOT NULL,
-    type TEXT NOT NULL,
     test_run_id INTEGER NOT NULL,
     iteration INTEGER NOT NULL,
+    execution_time_ms INTEGER NOT NULL
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    query_name VARCHAR(255) NOT NULL,
     explain JSONB NOT NULL,
-    execution_time_ms INTEGER NOT NULL
+    type TEXT NOT NULL,
 );
 
 CREATE SEQUENCE IF NOT EXISTS test_run_id_seq AS INTEGER
