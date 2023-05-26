5fedc7e1e806c368a0e3850204c50445d5f99195
LauJosefsen <ljosef18@student.aau.dk>
2023-05-26 22:15:29 +0000

    Update 2 files
    - /benchmarks/queries/cell/trajectory_query.sql
    - /benchmarks/queries/cell/cell_query.sql

--- benchmarks/queries/cell/cell_query.sql
+++ benchmarks/queries/cell/cell_query.sql
@@ -1,17 +1,15 @@
 WITH
-    q_window(box, start_date_id, end_date_id) AS (
+    q_window(box) AS (
         SELECT
             STBox(
                 ST_MakeEnvelope(:XMIN,:YMIN, :XMAX, :YMAX, 3034),
                 span(timestamp_from_date_time_id(:START_ID, 0), timestamp_from_date_time_id(:END_ID, 0), true, false)
-            ) box,
-            :START_ID start_date_id,
-            :END_ID end_date_id
+            ) box
     )
 SELECT distinct(ds.*)
 FROM q_window q
 INNER JOIN fact_cell_{CELL_SIZE}m fc ON fc.st_bounding_box && q.box
 INNER JOIN dim_ship ds ON ds.ship_id = fc.ship_id
 INNER JOIN dim_ship_type dst on ds.ship_type_id = dst.ship_type_id
-WHERE fc.entry_date_id BETWEEN q.start_date_id AND q.end_date_id AND dst.ship_type = ANY(:SHIP_TYPES)
+WHERE fc.entry_date_id BETWEEN :START_ID AND :END_ID AND dst.ship_type = ANY(:SHIP_TYPES)
 ;
--- benchmarks/queries/cell/trajectory_query.sql
+++ benchmarks/queries/cell/trajectory_query.sql
@@ -1,12 +1,10 @@
 WITH
-    q_window(box, start_date_id, end_date_id) AS (
+    q_window(box) AS (
         SELECT
             STBox(
                 ST_Transform(ST_MakeEnvelope(:XMIN,:YMIN, :XMAX, :YMAX, 3034), 4326),
                 span(timestamp_from_date_time_id(:START_ID, 0), timestamp_from_date_time_id(:END_ID, 0), true, false)
-            ) box,
-            :START_ID start_date_id,
-            :END_ID end_date_id
+            ) box
     )
 SELECT DISTINCT(ds.*)
 FROM q_window
@@ -14,5 +12,7 @@ INNER JOIN dim_trajectory dt ON setSrid(q_window.box, 4326) && dt.trajectory AND
 INNER JOIN fact_trajectory ft ON dt.date_id = ft.start_date_id AND dt.trajectory_sub_id = ft.trajectory_sub_id
 INNER JOIN dim_ship ds ON ds.ship_id = ft.ship_id
 INNER JOIN dim_ship_type dst on ds.ship_type_id = dst.ship_type_id
-WHERE ft.start_date_id BETWEEN q_window.start_date_id AND q_window.end_date_id AND dst.ship_type = ANY(:SHIP_TYPES)
+WHERE ft.start_date_id BETWEEN :START_ID AND :END_ID
+  AND dt.date_id BETWEEN :START_ID AND :END_ID
+  AND dst.ship_type = ANY(:SHIP_TYPES)
 ;
